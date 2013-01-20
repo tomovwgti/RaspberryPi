9ac9e6b98adf28980aa2b69efc0aaa6b3231193b
Tomo Watanabe <tomovwgti@gmail.com>
2013-01-20 23:37:09 +0900

    apply LED

--- server/app.js
+++ server/app.js
@@ -13,6 +13,8 @@ var app = express()
     , server = http.createServer(app)
     , io = require('socket.io').listen(server);
 
+// Socket.IO Log Level
+io.set('log level', 1);
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -32,6 +34,17 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
+// GPIO Peripheral
+// Full Color LED
+// R - GPIO4 : 23
+// G - GPIO3 : 24
+// B - GPIO1 : 18 : PWM
+var exec = require('child_process').exec;
+
+exec('gpio -g mode 18 pwm');
+exec('gpio -g mode 23 write');
+exec('gpio -g mode 24 write');
+
 // ソケットを作る
 var socketIO = require('socket.io');
 // クライアントの接続を待つ
@@ -43,8 +56,27 @@ io.sockets.on('connection', function(socket) {
     // メッセージを受けた時の処理
     socket.on('message', function(data) {
         // 接続していクライアントに全てに送信
-        console.log("value: " + data.value);
+        //console.log("value: " + data.value);
         socket.broadcast.emit('message', { value: data.value });
+        // B
+        //console.log("B: " + data.value % 1024);
+        exec('gpio -g pwm 18 ' + data.value % 1024);
+        // G
+        if ((1024 <= data.value && data.value < 2047) || (3072 <= data.value && data.value < 4095) ) {
+            //console.log("G: " + 1);
+            exec('gpio -g write 23 1');
+        } else {
+            //console.log("G: " + 0);
+            exec('gpio -g write 23 0');
+        }
+        // R
+        if ((2048 <= data.value && data.value < 3071) || (3072 <= data.value && data.value < 4095) ) {
+            //console.log("R: " + 1);
+            exec('gpio -g write 24 1');
+        } else {
+            //console.log("R: " + 0);
+            exec('gpio -g write 24 0');
+        }
     });
 
     // クライアントが切断した時に処理
