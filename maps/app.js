10bbe51cca21b28e9ffa5781fdb9eaaa94374613
Tomo Watanabe <tomovwgti@gmail.com>
2013-06-08 23:29:22 +0900

    Add nmea.js

--- maps/app.js
+++ maps/app.js
@@ -6,13 +6,24 @@
 var express = require('express')
     , routes = require('./routes')
     , http = require('http')
-    , path = require('path');
+    , path = require('path')
+    , serialport = require('serialport');
 
 var app = express()
     , http = require('http')
     , server = http.createServer(app)
     , io = require('socket.io').listen(server);
 
+// Serial Port
+var portName = '/dev/tty.PL2303-000013FA';
+var sp = new serialport.SerialPort(portName, {
+    baudRate: 38400,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: true,
+    parser: serialport.parsers.readline("\n")
+});
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -32,8 +43,6 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 
-// ソケットを作る
-var socketIO = require('socket.io');
 // クライアントの接続を待つ(IPアドレスとポート番号を結びつけます)
 server.listen(3000);
 
@@ -52,3 +61,43 @@ io.sockets.on('connection', function(socket) {
         console.log("disconnect");
     });
 });
+
+// data from Serial port
+sp.on('data', function(input) {
+
+    var buffer = new Buffer(input, 'utf8');
+    var receive = buffer.toString();
+//    console.log(receive);
+    if (receive.indexOf('$GPGGA') === -1) {
+        return;
+    }
+    // NMEAの解析
+    CaliculateNmea(receive);
+       // つながっているクライアント全員に送信
+//    io.sockets.json.emit('message', { value: jsonData });
+});
+
+sp.on('close', function(err) {
+    console.log('port closed');
+});
+
+sp.on('open', function(err) {
+    console.log('port opened');
+});
+
+// NMEAの解析
+function CaliculateNmea(receive) {
+    console.log(receive);
+    var data = receive.split(',');
+    var time = data[1];
+    var n = 3540.8581;//data[2];
+    var e = 13945.9603;//data[4];
+    var valid = data[6];
+    console.log('Time: ' + time);
+    console.log('N: ' + n);
+    console.log('E: ' + e);
+    console.log('Valid: ' + valid);
+    var deg = n.substring(0,2);
+    var min = n.substring(2,4);
+    var sec = n.substring()
+}
---
+++ maps/node_modules/nmea/nmea.js
@@ -0,0 +1,128 @@
+// A NMEA-0183 parser based on the format given here: http://www.tronico.fi/OH6NT/docs/NMEA0183.pdf
+
+var validLine = function(line) {
+    // check that the line passes checksum validation
+    // checksum is the XOR of all characters between $ and * in the message.
+    // checksum reference is provided as a hex value after the * in the message.
+    var checkVal = 0;
+    var parts = line.split('*');
+    for (var i=1; i < parts[0].length; i++) {
+        checkVal = checkVal ^ parts[0].charCodeAt(i);
+    };
+    return checkVal == parseInt(parts[1], 16);
+};
+
+exports.parsers = {
+    GGA: function(fields) {
+        var FIX_TYPE = ['none', 'fix', 'delta'];
+        return {
+            type: 'fix',
+            timestamp: fields[1],
+            lat: fields[2],
+            latPole: fields[3],
+            lon: fields[4],
+            lonPole: fields[5],
+            fixType: FIX_TYPE[+fields[6]],
+            numSat: +fields[7],
+            horDilution: +fields[8],
+            alt: +fields[9],
+            altUnit: fields[10],
+            geoidalSep: +fields[11],
+            geoidalSepUnit: fields[12],
+            differentialAge: +fields[13],
+            differentialRefStn: fields[14],
+        };
+    },
+    GLL: function(fields) {
+        return {
+            type: 'geo-position',
+            timestamp: fields[5],
+            lat: fields[2],
+            latPole: fields[3],
+            lon: fields[4],
+            lonPole: fields[5],
+            status: fields[6] == 'A' ? 'valid' : 'invalid'
+        };
+    },
+    RMC: function(fields) {
+        return {
+            type: 'nav-info',
+            timestamp: fields[1],
+            status: fields[2] == 'V' ? 'warning' : 'valid',
+            lat: fields[3],
+            latPole: fields[4],
+            lon: fields[5],
+            lonPole: fields[6],
+            speedKnots: +fields[7],
+            trackTrue: +fields[8],
+            date: fields[9],
+            variation: +fields[10],
+            variationPole: fields[11]
+        };
+    },
+    VTG: function(fields) {
+        return {
+            type: 'track-info',
+            trackTrue: +fields[1],
+            trackMagnetic: +fields[3],
+            speedKnots: +fields[5],
+            speedKmph: +fields[7]
+        };
+    },
+    GSA: function(fields) {
+      // $GPGSA,A,3,12,05,25,29,,,,,,,,,9.4,7.6,5.6
+       var sats = [];
+       for (var i=1; i < 13; i++) {
+           if (fields[i+2]) sats.push(+fields[i+2]);
+       };
+       return {
+          type: 'active-satellites',
+          selectionMode: fields[1],
+          mode: +fields[2],
+          satellites: sats,
+          PDOP: fields[15],
+          HDOP: fields[16],
+          VDOP: fields[17]
+      };
+    },
+    GSV: function(fields) {
+        // $GPGSV,3,1,12, 05,58,322,36, 02,55,032,, 26,50,173,, 04,31,085,
+        var numRecords = (fields.length - 4) / 4,
+            sats = [];
+        for (var i=0; i < numRecords; i++) {
+            var offset = i * 4 + 4;
+            sats.push({id: fields[offset],
+                       elevationDeg: +fields[offset+1],
+                       azimuthTrue: +fields[offset+2],
+                       SNRdB: +fields[offset+3]});
+        };
+        return {
+            type: 'satellite-list-partial',
+            numMsgs: +fields[1],
+            msgNum: +fields[2],
+            satsInView: +fields[3],
+            satellites: sats
+        };
+    }
+};
+
+exports.parse = function(line) {
+    if (validLine(line)) {
+        var fields = line.split('*')[0].split(','),
+            talker_id,
+            msg_fmt;
+        if (fields[0].charAt(1) == 'P') {
+            talker_id = 'P'; // Proprietary
+            msg_fmt = fields[0].substr(2);
+        } else {
+            talker_id = fields[0].substr(1, 2);
+            msg_fmt = fields[0].substr(3);
+        }
+        parser = exports.parsers[msg_fmt];
+        if (parser) {
+            var val = parser(fields);
+            val.talker_id = talker_id;
+            return val;   
+        }
+    }
+};
